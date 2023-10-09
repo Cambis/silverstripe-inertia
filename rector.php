95988b1f389132c34c2d798243728cf844bdc56c
Cameron Bryers <12287346+Cambis@users.noreply.github.com>
2023-10-10 12:45:24 +1300

    feat: add phpstan and rector

---
+++ rector.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\Set\ValueObject\SetList;
+use Netwerkstatt\SilverstripeRector\Set\SilverstripeSetList;
+use Netwerkstatt\SilverstripeRector\Set\SilverstripeLevelSetList;
+use Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector;
+use Rector\Php81\Rector\Property\ReadOnlyPropertyRector;
+use Rector\Php74\Rector\Closure\ClosureToArrowFunctionRector;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->paths([
+        __DIR__ . '/_config.php',
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ]);
+
+    $rectorConfig->sets([
+        SetList::CODE_QUALITY,
+        SetList::CODING_STYLE,
+        SetList::DEAD_CODE,
+        LevelSetList::UP_TO_PHP_81,
+        SilverstripeLevelSetList::UP_TO_SS_5_1,
+        SilverstripeSetList::CODE_STYLE,
+    ]);
+
+    $rectorConfig->skip([
+        ClassPropertyAssignToConstructorPromotionRector::class,
+        ClosureToArrowFunctionRector::class,
+        ReadOnlyPropertyRector::class,
+    ]);
+};
