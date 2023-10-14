682785d49b503c16b2a72b2bac27abc1f5debe3a
Cameron Bryers <12287346+Cambis@users.noreply.github.com>
2023-10-15 12:35:08 +1300

    revert: remove silverstripe-rector
    - remove silverstripe-rector, package contained stubs with incorrect method definitions. Will look to reintroduce at a later stage.

--- rector.php
+++ rector.php
@@ -2,12 +2,6 @@
 
 declare(strict_types=1);
 
-use Cambis\Inertia\Inertia;
-use Cambis\Inertia\Extension\InertiaPageControllerExtension;
-use Netwerkstatt\SilverstripeRector\Rector\Misc\AddConfigPropertiesRector;
-use Netwerkstatt\SilverstripeRector\Rector\Injector\UseCreateRector;
-use Netwerkstatt\SilverstripeRector\Set\SilverstripeLevelSetList;
-use Netwerkstatt\SilverstripeRector\Set\SilverstripeSetList;
 use Rector\Config\RectorConfig;
 use Rector\Php74\Rector\Closure\ClosureToArrowFunctionRector;
 use Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector;
@@ -24,32 +18,11 @@ return static function (RectorConfig $rectorConfig): void {
         __DIR__ . '/tests',
     ]);
 
-    $rectorConfig->rules([
-        UseCreateRector::class,
-    ]);
-
-    $rectorConfig->ruleWithConfiguration(
-        AddConfigPropertiesRector::class,
-        [
-            Inertia::class => [
-                'manifest_file',
-                'root_view',
-                'ssr_enabled',
-                'ssr_host',
-            ],
-            InertiaPageControllerExtension::class => [
-                'dependencies',
-            ],
-        ],
-    );
-
     $rectorConfig->sets([
         LevelSetList::UP_TO_PHP_81,
         SetList::CODE_QUALITY,
         SetList::CODING_STYLE,
         SetList::DEAD_CODE,
-        SilverstripeLevelSetList::UP_TO_SS_5_1,
-        SilverstripeSetList::CODE_STYLE,
     ]);
 
     $rectorConfig->skip([
